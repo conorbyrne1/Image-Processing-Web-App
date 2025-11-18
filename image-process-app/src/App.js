59dfd365c1c34b0af1e724b20049fb031c82d2be
Conor Byrne <conor.byrne@mymail.champlain.edu>
2025-11-18 18:30:53 -0500

    fix for pages #3

--- image-process-app/src/App.js
+++ image-process-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home";
-import ImageBlur from "./pages/ImageBlur";
-import EdgeDetection from "./pages/EdgeDetection";
-import Sharpen from "./pages/Sharpen";
-import ContrastAdjustment from "./pages/ContrastAdjustment";
-import BrightnessAdjustment from "./pages/BrightnessAdjustment";
-import GrayscaleConversion from "./pages/GrayscaleConversion";
+import Home from ".//pages/Home";
+import ImageBlur from ".//pages/ImageBlur";
+import EdgeDetection from ".//pages/EdgeDetection";
+import Sharpen from ".//pages/Sharpen";
+import ContrastAdjustment from ".//pages/ContrastAdjustment";
+import BrightnessAdjustment from ".//pages/BrightnessAdjustment";
+import GrayscaleConversion from ".//pages/GrayscaleConversion";
 import './App.css';
 
 function App() {
